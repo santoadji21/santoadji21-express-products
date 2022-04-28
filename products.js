d3259e749261496b9ac0bf1ac6c503ab22587c2a
santoadji21 <ahmad.aji.santoso21@gmail.com>
2022-04-28 17:08:01 +0700

    initial

---
+++ products.js
@@ -0,0 +1,47 @@
+ const products = [
+  {
+    id: 1,
+    name: "Basic Tee",
+    secname: "Basic-Tee",
+    href: "#",
+    price: 35.52,
+    color: "Color Red",
+    inStock: true,
+    size: "Size-M",
+    quantity: 1,
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-01.jpg",
+    imageAlt: "Front of men's Basic Tee in sienna.",
+  },
+  {
+    id: 2,
+    name: "Basic Tee",
+    secname: "Basic-Tee",
+    href: "#",
+    price: 32.57,
+    quantity: 1,
+    color: "Color Red",
+    inStock: false,
+    leadTime: "3–4 weeks",
+    size: "Size-XL",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-02.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+  },
+  {
+    id: 3,
+    name: "Nomad Tumbler",
+    secname: "Nomad-Tumbler",
+    href: "#",
+    price: 58.9,
+    color: "Color Red",
+    inStock: true,
+    size: "Size-M",
+    quantity: 1,
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-03.jpg",
+    imageAlt: "Insulated bottle with white base and black snap lid.",
+  },
+];
+
+module.exports = products;
